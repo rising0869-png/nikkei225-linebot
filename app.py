3dbc517dc39e500185cab570dddb370b19dfd04c
rising0869-png <rising0869@gmail.com>
2026-06-04 22:00:19 +0900

    add webhook

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "225BOT OK"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    body = request.get_json()
+
+    print(body)
+
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=True)
