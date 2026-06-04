5231db6d08c45320d94f45cad112e8b24884aa50
rising0869-png <rising0869@gmail.com>
2026-06-04 22:37:48 +0900

    stock search

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+import gspread
+
+gc = gspread.service_account(
+    filename="bot-498411-78d452146270.json"
+)
+
+sheet = gc.open("日経平均225").sheet1
+
+def search_stock(keyword):
+    for row in rows:
+        code = str(row["銘柄コード"])
+        name = row["銘柄名"]
+
+        if keyword in code or keyword in name:
+            return (
+                f"{code}\n"
+                f"{name}\n"
+                f"業種：{row['業種']}"
+            )
+
+    return "見つかりませんでした"
+
+rows = sheet.get_all_records()
 from flask import Flask, request
 import os
 from linebot.v3 import WebhookHandler
@@ -12,8 +35,8 @@ from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 
-CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET="mCDUULpq/8U3UDXhFIXSWRMYBuvJD6XM+LFGqikNeHjDuMo+oRSrT2jLiDFKh0OpDcPv9Mjl9qIXc6jL47LhExEjWHxMBzGjg/n+6rJgBUoJCwV0twQYLUbk3/srwrbY0uvcgIXx7MLcn8hGxuOAJwdB04t89/1O/w1cDnyilFU="
+CHANNEL_ACCESS_TOKEN ="bfe03207ce7d9df623aafc4432d86fff"
 
 configuration = Configuration(
     access_token=CHANNEL_ACCESS_TOKEN
@@ -38,6 +61,8 @@ def webhook():
 def handle_message(event):
     user_text = event.message.text
 
+    reply_text = search_stock(user_text)
+
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
 
@@ -46,7 +71,7 @@ def handle_message(event):
                 reply_token=event.reply_token,
                 messages=[
                     TextMessage(
-                        text=f"受信: {user_text}"
+                        text=reply_text
                     )
                 ]
             )
